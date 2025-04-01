425804124e6408308bb3c379bb0667c98f2c129f
Gowthamijainn <123972050+Gowthamijainn@users.noreply.github.com>
2025-04-01 08:02:32 +0530

    next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",  // Enables static export
+  basePath: "/Janu_22_bday", // Use your GitHub repo name
+  images: { unoptimized: true }, // Required for GitHub Pages
+};
 
 export default nextConfig;
